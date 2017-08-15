2ee507ac85280e5f4c58683b882f8c97d7744220
michellibrito <michellidibrito@gmail.com>
2017-08-14 21:42:20 -0300

    second commit

--- src/main/java/agendaonline/com/controllers/PacientesController.java
+++ src/main/java/agendaonline/com/controllers/PacientesController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -47,5 +48,14 @@ public class PacientesController {
 		pr.save(paciente);
 		System.out.println(paciente.getConvenio());
 		return "redirect:/pacientes";
+	}
+	
+	@RequestMapping(value="/{nome}", method = RequestMethod.GET)
+	public ModelAndView detalhes(@PathVariable("nome") String nome){
+		ModelAndView mv = new ModelAndView("pacientes/pacienteDetalhes");
+		Paciente paciente = pr.findOne(nome);
+		mv.addObject("paciente", paciente);
+		System.out.println(paciente.getEmail());
+		return mv;
 	}
 }
